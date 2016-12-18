1b8ad7146f3ec81aa393fcf4361b8323761db6ac
Matt Thomson <mattjohnthomson@gmail.com>
2016-12-18 12:55:27 +0000

    Add day 17 part 2

--- 2016/day17/lib/day17.ex
+++ 2016/day17/lib/day17.ex
@@ -9,15 +9,31 @@ defmodule Day17 do
     [input |> Position.new] |> solve_part_one
   end
 
+  def part_two(input) do
+    [input |> Position.new] |> solve_part_two(0)
+  end
+
   defp solve_part_one([position | rest]) do
     if position.coordinates == {3, 3} do
       position.directions |> Directions.to_string
     else
-      new_positions =
-        position
-        |> Position.valid_directions
-        |> Enum.map(&Position.step(position, &1))
-      solve_part_one(rest ++ new_positions)
+      solve_part_one(rest ++ (position |> new_positions))
     end
+  end
+
+  defp solve_part_two([position | rest], acc) do
+    if position.coordinates == {3, 3} do
+      new_acc = max(acc, position.directions |> Enum.count)
+      solve_part_two(rest, new_acc)
+    else
+      solve_part_two(rest ++ (position |> new_positions), acc)
+    end
+  end
+  defp solve_part_two([], acc), do: acc
+
+  defp new_positions(position) do
+    position
+    |> Position.valid_directions
+    |> Enum.map(&Position.step(position, &1))
   end
 end
--- 2016/day17/test/day17_test.exs
+++ 2016/day17/test/day17_test.exs
@@ -6,4 +6,10 @@ defmodule Day17Test do
     assert Day17.part_one("kglvqrro") == "DDUDRLRRUDRD"
     assert Day17.part_one("ulqzkmiv") == "DRURDRUDDLLDLUURRDULRLDUUDDDRR"
   end
+
+  test "solves part two" do
+    assert Day17.part_two("ihgpwlah") == 370
+    assert Day17.part_two("kglvqrro") == 492
+    assert Day17.part_two("ulqzkmiv") == 830
+  end
 end

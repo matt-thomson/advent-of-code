1474d56d7054acf20408431f3f1c265fa37f6ef0
Matt Thomson <mattjohnthomson@gmail.com>
2017-12-02 10:47:01 +0000

    Day 2 part 2

--- 2017/lib/advent_of_code2017/day02.rb
+++ 2017/lib/advent_of_code2017/day02.rb
@@ -3,15 +3,33 @@
 module AdventOfCode2017
   module Day02
     def self.solve_part_one(input)
-      input.split("\n")
-           .map { |line| score(line) }
-           .inject(0) { |sum, x| sum + x }
+      solve(input) { |numbers| score_part_one(numbers) }
     end
 
-    def self.score(line)
-      numbers = line.split.map(&:to_i)
+    def self.solve_part_two(input)
+      solve(input) { |numbers| score_part_two(numbers) }
+    end
+
+    def self.solve(input)
+      input.split("\n")
+           .map { |line| line.split.map(&:to_i) }
+           .map { |numbers| yield(numbers) }
+           .inject(0) { |sum, x| sum + x }
+    end
+    private_class_method :solve
+
+    def self.score_part_one(numbers)
       numbers.max - numbers.min
     end
-    private_class_method :score
+    private_class_method :score_part_one
+
+    def self.score_part_two(numbers)
+      x, y = numbers.product(numbers)
+                    .reject { |x, y| x <= y }
+                    .find { |x, y| (x % y).zero? }
+
+      x / y
+    end
+    private_class_method :score_part_two
   end
 end
--- 2017/spec/advent_of_code2017/day02_spec.rb
+++ 2017/spec/advent_of_code2017/day02_spec.rb
@@ -16,4 +16,18 @@ RSpec.describe AdventOfCode2017::Day02 do
 
     it { is_expected.to eq(18) }
   end
+
+  describe '.solve_part_two' do
+    subject { described_class.solve_part_two(input) }
+
+    let(:input) do
+      <<~INPUT
+        5 9 2 8
+        9 4 7 3
+        3 8 6 5
+        INPUT
+    end
+
+    it { is_expected.to eq(9) }
+  end
 end

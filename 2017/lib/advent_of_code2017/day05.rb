feff903151284240a9d3f0c84cdfe52d8d11ef06
Matt Thomson <mattjohnthomson@gmail.com>
2017-12-07 18:47:05 +0000

    Day 5 part 2

--- 2017/lib/advent_of_code2017/day05.rb
+++ 2017/lib/advent_of_code2017/day05.rb
@@ -3,6 +3,20 @@
 module AdventOfCode2017
   module Day05
     def self.solve_part_one(input)
+      solve(input) { |_offset| 1 }
+    end
+
+    def self.solve_part_two(input)
+      solve(input) do |offset|
+        if offset >= 3
+          - 1
+        else
+          1
+        end
+      end
+    end
+
+    def self.solve(input)
       jumps = input.split.map(&:to_i)
 
       count = 0
@@ -14,12 +28,13 @@ module AdventOfCode2017
 
         count += 1
 
-        old_jumps = jumps[position]
-        jumps[position] += 1
-        position += old_jumps
+        offset = jumps[position]
+        jumps[position] += yield(offset)
+        position += offset
       end
 
       count
     end
+    private_class_method :solve
   end
 end
--- 2017/spec/advent_of_code2017/day05_spec.rb
+++ 2017/spec/advent_of_code2017/day05_spec.rb
@@ -18,4 +18,20 @@ RSpec.describe AdventOfCode2017::Day05 do
 
     it { is_expected.to eq(5) }
   end
+
+  describe '.solve_part_two' do
+    subject { described_class.solve_part_two(input) }
+
+    let(:input) do
+      <<~INPUT
+        0
+        3
+        0
+        1
+        -3
+      INPUT
+    end
+
+    it { is_expected.to eq(10) }
+  end
 end

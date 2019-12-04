38f98aa509c5ef9284fa504c21693929914389f2
Matt Thomson <matt-thomson@users.noreply.github.com>
2019-12-04 08:53:53 +0000

    Day 4 part 2

--- src/day04.rs
+++ src/day04.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use structopt::StructOpt;
 
 use crate::command;
@@ -10,13 +11,17 @@ pub struct Day04 {
 
 impl command::Command for Day04 {
     fn part_one(&self) -> u32 {
-        (self.start..=self.end)
-            .filter(|x| valid_password(*x))
-            .count() as u32
+        self.solve(valid_password_part_one)
     }
 
     fn part_two(&self) -> u32 {
-        unimplemented!()
+        self.solve(valid_password_part_two)
+    }
+}
+
+impl Day04 {
+    fn solve(&self, predicate: fn(u32) -> bool) -> u32 {
+        (self.start..=self.end).filter(|x| predicate(*x)).count() as u32
     }
 }
 
@@ -33,13 +38,26 @@ fn digits(input: u32) -> Vec<u32> {
     result
 }
 
-fn valid_password(password: u32) -> bool {
+fn valid_password_part_one(password: u32) -> bool {
     let digits = digits(password);
     has_double(&digits) && never_decrease(&digits)
 }
 
+fn valid_password_part_two(password: u32) -> bool {
+    let digits = digits(password);
+    has_exact_double(&digits) && never_decrease(&digits)
+}
+
 fn has_double(digits: &[u32]) -> bool {
     digits.windows(2).any(|pair| pair[0] == pair[1])
+}
+
+fn has_exact_double(digits: &[u32]) -> bool {
+    digits
+        .iter()
+        .group_by(|x| *x)
+        .into_iter()
+        .any(|(_, group)| group.count() == 2)
 }
 
 fn never_decrease(digits: &[u32]) -> bool {
@@ -58,10 +76,17 @@ mod tests {
     }
 
     #[test]
-    fn test_valid_password() {
-        assert!(valid_password(111_111));
-        assert!(!valid_password(223_450));
-        assert!(!valid_password(123_789));
+    fn test_valid_password_part_one() {
+        assert!(valid_password_part_one(111_111));
+        assert!(!valid_password_part_one(223_450));
+        assert!(!valid_password_part_one(123_789));
+    }
+
+    #[test]
+    fn test_valid_password_part_two() {
+        assert!(valid_password_part_two(112_233));
+        assert!(!valid_password_part_two(123_444));
+        assert!(valid_password_part_two(111_122));
     }
 
     #[test]
@@ -72,5 +97,15 @@ mod tests {
         };
 
         assert_eq!(command.part_one(), 13);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let command = Day04 {
+            start: 300,
+            end: 400,
+        };
+
+        assert_eq!(command.part_two(), 12);
     }
 }

41f7e599f6beab031b0dd8a9fe15c1d6285f8f58
Matt Thomson <matt-thomson@users.noreply.github.com>
2019-12-08 14:01:58 +0000

    Day 8 part 2

--- src/day08.rs
+++ src/day08.rs
@@ -2,6 +2,7 @@ use std::fs;
 use std::path::PathBuf;
 
 use bytecount;
+use itertools::Itertools;
 use structopt::StructOpt;
 
 use crate::command;
@@ -30,13 +31,53 @@ impl command::Command for Day08 {
     }
 
     fn part_two(&self) -> u32 {
-        unimplemented!();
+        for row in self.image() {
+            println!("{}", format_row(&row));
+        }
+
+        0
+    }
+}
+
+impl Day08 {
+    fn image(&self) -> Vec<Vec<bool>> {
+        let input = fs::read_to_string(&self.input).unwrap();
+        let bytes = input.trim().as_bytes();
+
+        let mut image = vec![];
+
+        for i in 0..self.height {
+            let mut row = vec![];
+
+            for j in 0..self.width {
+                let mut position = i * self.width + j;
+
+                loop {
+                    let pixel = bytes[position];
+
+                    if pixel != 50 {
+                        row.push(pixel == 48);
+                        break;
+                    }
+
+                    position += self.width * self.height;
+                }
+            }
+
+            image.push(row);
+        }
+
+        image
     }
 }
 
 fn count(bytes: &[u8], digit: u8) -> usize {
     let target = digit + 48;
     bytecount::count(bytes, target)
+}
+
+fn format_row(row: &[bool]) -> String {
+    row.iter().map(|x| if *x { ' ' } else { '■' }).join("")
 }
 
 #[cfg(test)]
@@ -52,8 +93,28 @@ mod tests {
     }
 
     #[test]
+    fn test_image() {
+        let input = PathBuf::from("fixtures/day08b.txt");
+
+        let command = Day08 {
+            input,
+            width: 2,
+            height: 2,
+        };
+
+        assert_eq!(command.image(), vec![vec![true, false], vec![false, true]]);
+    }
+
+    #[test]
+    fn test_format_row() {
+        let row = vec![true, false, true, false, true];
+        assert_eq!(format_row(&row), " ■ ■ ");
+    }
+
+    #[test]
     fn test_part_one() {
-        let input = PathBuf::from("fixtures/day08.txt");
+        let input = PathBuf::from("fixtures/day08a.txt");
+
         let command = Day08 {
             input,
             width: 3,

d5ed110ecbd6cd772cac453f1d4da066beb2f982
Matt Thomson <matt-thomson@users.noreply.github.com>
2019-12-20 17:52:29 +0000

    Find portals

--- src/day20/maze.rs
+++ src/day20/maze.rs
@@ -7,7 +7,7 @@ pub type Position = (usize, usize);
 
 #[derive(Debug)]
 pub struct Maze {
-    walls: HashSet<Position>,
+    spaces: HashSet<Position>,
 }
 
 impl Maze {
@@ -15,25 +15,45 @@ impl Maze {
         let file = File::open(path).unwrap();
         let reader = BufReader::new(file);
 
-        let mut walls = HashSet::new();
+        let mut spaces = HashSet::new();
         let mut letters = HashMap::new();
 
         for (y, row) in reader.lines().enumerate() {
             for (x, position) in row.unwrap().chars().enumerate() {
                 match position {
-                    '#' => {
-                        walls.insert((x, y));
-                    }
+                    '#' => (),
                     i if i.is_ascii_uppercase() => {
                         letters.insert((x, y), i);
                     }
-                    '.' => (),
+                    '.' => {
+                        spaces.insert((x, y));
+                    }
                     ' ' => (),
                     _ => panic!("unknown position {}", position),
                 }
             }
         }
 
-        Self { walls }
+        let mut portals: Vec<(Position, (char, char))> = vec![];
+
+        for (&(x, y), i) in &letters {
+            if let Some(&other) = letters.get(&(x, y + 1)) {
+                if spaces.contains(&(x, y + 2)) {
+                    portals.push(((x, y + 2), (*i, other)));
+                } else if spaces.contains(&(x, y - 1)) {
+                    portals.push(((x, y - 1), (*i, other)));
+                }
+            } else if let Some(&other) = letters.get(&(x + 1, y)) {
+                if spaces.contains(&(x + 2, y)) {
+                    portals.push(((x + 2, y), (*i, other)));
+                } else if spaces.contains(&(x - 1, y)) {
+                    portals.push(((x - 1, y), (*i, other)));
+                }
+            }
+        }
+
+        println!("{:?}", portals);
+
+        Self { spaces }
     }
 }

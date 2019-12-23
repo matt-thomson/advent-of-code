00ac6e29541dc80a1e5720ac6e289c16a5c3c402
Matt Thomson <matt-thomson@users.noreply.github.com>
2019-12-23 21:08:37 +0000

    Day 23 part 1

--- src/day23/mod.rs
+++ src/day23/mod.rs
@@ -1,8 +1,18 @@
+mod node;
+mod packet;
+
+use std::collections::HashMap;
 use std::path::PathBuf;
 
 use structopt::StructOpt;
 
+use crate::intcode::Program;
 use crate::problem::Problem;
+
+use node::Node;
+use packet::Packet;
+
+const NUM_NODES: usize = 50;
 
 #[derive(Debug, StructOpt)]
 pub struct Day23 {
@@ -14,7 +24,29 @@ impl Problem for Day23 {
     type Output = i64;
 
     fn part_one(&self) -> i64 {
-        unimplemented!();
+        let program = Program::read(&self.input);
+        let mut nodes: Vec<Node> = (0..NUM_NODES)
+            .map(|address| Node::new(&program, address))
+            .collect();
+
+        let mut packets: HashMap<usize, Vec<Packet>> = HashMap::new();
+
+        loop {
+            for (address, node) in nodes.iter_mut().enumerate() {
+                let input = packets.remove(&address).unwrap_or_else(|| vec![]);
+
+                for packet in node.run(&input) {
+                    if packet.address() == 255 {
+                        return packet.y();
+                    }
+
+                    packets
+                        .entry(packet.address())
+                        .or_insert_with(|| vec![])
+                        .push(packet);
+                }
+            }
+        }
     }
 
     fn part_two(&self) -> i64 {
---
+++ src/day23/node.rs
@@ -0,0 +1,29 @@
+use crate::intcode::{Computer, Program};
+
+use super::packet::Packet;
+
+pub struct Node {
+    computer: Computer,
+}
+
+impl Node {
+    pub fn new(program: &Program, address: usize) -> Self {
+        let mut computer = program.launch();
+        computer.run(&[address as i64]);
+
+        Node { computer }
+    }
+
+    pub fn run(&mut self, input: &[Packet]) -> Vec<Packet> {
+        let mut input: Vec<i64> = input
+            .iter()
+            .flat_map(|packet| vec![packet.x(), packet.y()])
+            .collect();
+
+        input.push(-1);
+
+        let output = self.computer.run(&input);
+
+        output.chunks(3).map(|chunk| Packet::from(&chunk)).collect()
+    }
+}

7a137a91faa424afdc8554e2cbea2008630f9bd5
Matt Thomson <matt-thomson@users.noreply.github.com>
2019-12-24 13:55:53 +0000

    Step from one board to next

--- src/day24/board.rs
+++ src/day24/board.rs
@@ -3,6 +3,8 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 
+const SIZE: usize = 5;
+
 type Position = (usize, usize);
 
 pub struct Board {
@@ -24,6 +26,47 @@ impl Board {
         }
 
         Self { bugs }
+    }
+
+    pub fn step(&self) -> Self {
+        let mut bugs = HashSet::new();
+
+        for x in 0..SIZE {
+            for y in 0..SIZE {
+                let position = (x, y);
+                let neighbours = self.neighbours(&position);
+                let alive = self.bugs.contains(&position);
+
+                if neighbours == 1 || (neighbours == 2 && !alive) {
+                    bugs.insert(position);
+                }
+            }
+        }
+
+        Self { bugs }
+    }
+
+    fn neighbours(&self, position: &Position) -> usize {
+        let mut count = 0;
+        let (x, y) = *position;
+
+        if x != 0 && self.bugs.contains(&(x - 1, y)) {
+            count += 1;
+        }
+
+        if y != 0 && self.bugs.contains(&(x, y - 1)) {
+            count += 1;
+        }
+
+        if x != SIZE - 1 && self.bugs.contains(&(x + 1, y)) {
+            count += 1;
+        }
+
+        if y != SIZE - 1 && self.bugs.contains(&(x, y + 1)) {
+            count += 1;
+        }
+
+        count
     }
 }
 
@@ -47,6 +90,32 @@ mod tests {
         expected.insert((4, 2));
         expected.insert((2, 3));
         expected.insert((0, 4));
+
+        assert_eq!(board.bugs, expected);
+    }
+
+    #[test]
+    fn test_step() {
+        let path = PathBuf::from("fixtures/day24.txt");
+        let board = Board::read(&path).step();
+
+        let mut expected = HashSet::new();
+        expected.insert((0, 0));
+        expected.insert((3, 0));
+        expected.insert((0, 1));
+        expected.insert((1, 1));
+        expected.insert((2, 1));
+        expected.insert((3, 1));
+        expected.insert((0, 2));
+        expected.insert((1, 2));
+        expected.insert((2, 2));
+        expected.insert((4, 2));
+        expected.insert((0, 3));
+        expected.insert((1, 3));
+        expected.insert((3, 3));
+        expected.insert((4, 3));
+        expected.insert((1, 4));
+        expected.insert((2, 4));
 
         assert_eq!(board.bugs, expected);
     }

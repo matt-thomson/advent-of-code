13d5304e8c47cd231ff555c6d96b381b875b351a
Matt Thomson <matt-thomson@users.noreply.github.com>
2019-12-24 13:28:01 +0000

    Skeleton for day 24

---
+++ src/day24/mod.rs
@@ -0,0 +1,23 @@
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+
+use crate::problem::Problem;
+
+#[derive(Debug, StructOpt)]
+pub struct Day24 {
+    #[structopt(parse(from_os_str))]
+    input: PathBuf,
+}
+
+impl Problem for Day24 {
+    type Output = u32;
+
+    fn part_one(&self) -> u32 {
+        unimplemented!();
+    }
+
+    fn part_two(&self) -> u32 {
+        unimplemented!();
+    }
+}

21a6c76dc5c1680da9e704febfe7dcad56f838e4
Matt Thomson <matt-thomson@users.noreply.github.com>
2019-12-24 19:39:58 +0000

    Day 24 part 1

--- src/day24/mod.rs
+++ src/day24/mod.rs
@@ -1,10 +1,13 @@
 mod board;
 
+use std::collections::HashSet;
 use std::path::PathBuf;
 
 use structopt::StructOpt;
 
 use crate::problem::Problem;
+
+use board::Board;
 
 #[derive(Debug, StructOpt)]
 pub struct Day24 {
@@ -16,10 +19,35 @@ impl Problem for Day24 {
     type Output = u32;
 
     fn part_one(&self) -> u32 {
-        unimplemented!();
+        let mut board = Board::read(&self.input);
+        let mut seen = HashSet::new();
+
+        loop {
+            let biodiversity = board.biodiversity();
+
+            if seen.contains(&biodiversity) {
+                return biodiversity;
+            }
+
+            seen.insert(biodiversity);
+            board = board.step();
+        }
     }
 
     fn part_two(&self) -> u32 {
         unimplemented!();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = PathBuf::from("fixtures/day24.txt");
+        let problem = Day24 { input };
+
+        assert_eq!(problem.part_one(), 2_129_920);
     }
 }

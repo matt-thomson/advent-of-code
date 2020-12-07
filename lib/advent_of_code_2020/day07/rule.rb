f4957d3a9fd04b887f234306bd1518bd5f2e7663
Matt Thomson <matt-thomson@users.noreply.github.com>
2020-12-07 09:49:24 +0000

    Add parser for conditions

---
+++ lib/advent_of_code_2020/day07/rule.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module AdventOfCode2020
+  module Day07
+    class Rule
+      RULE_REGEX = /\A(?<colour>[a-z ]+) bags contain (?<conditions>.*).\Z/.freeze
+      CONDITION_REGEX = /\A(?<count>\d+) (?<colour>[a-z ]+) bags?\Z/.freeze
+
+      def self.parse(input)
+        rule_matches = input.match(RULE_REGEX)
+        conditions = parse_conditions(rule_matches[:conditions])
+
+        new(rule_matches[:colour], conditions)
+      end
+
+      def self.parse_conditions(input)
+        return {} if input == 'no other bags'
+
+        input
+          .split(', ')
+          .map { |c| c.match(CONDITION_REGEX) }
+          .map { |match| [match[:colour], match[:count].to_i] }
+          .to_h
+      end
+      private_class_method :parse_conditions
+
+      def initialize(colour, conditions)
+        @colour = colour
+        @conditions = conditions
+      end
+
+      attr_reader :colour, :conditions
+    end
+  end
+end

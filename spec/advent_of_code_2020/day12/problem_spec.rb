7b5f731a0b2eeda472bba1bda84103a1215e67d5
Matt Thomson <matt-thomson@users.noreply.github.com>
2020-12-13 20:18:35 +0000

    Add day 12 part 2

--- spec/advent_of_code_2020/day12/problem_spec.rb
+++ spec/advent_of_code_2020/day12/problem_spec.rb
@@ -5,11 +5,17 @@ require 'advent_of_code_2020/day12/problem'
 RSpec.describe AdventOfCode2020::Day12::Problem do
   subject(:problem) { described_class.new(fixture_path) }
 
+  let(:fixture_path) { 'spec/fixtures/day12.txt' }
+
   describe '#part_one' do
     subject(:part_one) { problem.part_one }
 
-    let(:fixture_path) { 'spec/fixtures/day12.txt' }
-
     it { is_expected.to eq(25) }
+  end
+
+  describe '#part_tw0' do
+    subject(:part_one) { problem.part_two }
+
+    it { is_expected.to eq(286) }
   end
 end

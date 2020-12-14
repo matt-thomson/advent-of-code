910444247589a7a92783f54fe73bcafd8b155c76
Matt Thomson <matt-thomson@users.noreply.github.com>
2020-12-14 09:11:32 +0000

    Add day 14 part 1

---
+++ lib/advent_of_code_2020/day14/problem.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require_relative 'mask'
+
+module AdventOfCode2020
+  module Day14
+    class Problem
+      MASK_REGEX = /\Amask = (?<mask>[01X]+)\Z/.freeze
+      MEM_REGEX = /\Amem\[(?<address>\d+)\] = (?<value>\d+)\Z/.freeze
+
+      def initialize(path)
+        @instructions = File.readlines(path).map { |line| parse_instruction(line.strip) }
+      end
+
+      def part_one
+        memory = {}
+        mask = nil
+
+        @instructions.each do |instruction|
+          case instruction[0]
+          when :mask then mask = instruction[1]
+          when :mem then memory[instruction[1]] = mask.apply(instruction[2])
+          end
+        end
+
+        memory.values.sum
+      end
+
+      private
+
+      def parse_instruction(line)
+        if (mask_matches = line.match(MASK_REGEX))
+          [:mask, Mask.new(mask_matches[:mask])]
+        elsif (mem_matches = line.match(MEM_REGEX))
+          [:mem, mem_matches[:address].to_i, mem_matches[:value].to_i]
+        end
+      end
+    end
+  end
+end

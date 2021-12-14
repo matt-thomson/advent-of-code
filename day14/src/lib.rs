974fd656d4cac2c1d670376e5ab86ae7f48df396
Matt Thomson <771863+matt-thomson@users.noreply.github.com>
2021-12-14 10:30:17 +0000

    Day 14 part 2

--- day14/src/lib.rs
+++ day14/src/lib.rs
@@ -5,8 +5,10 @@ use std::path::Path;
 
 #[derive(Debug)]
 pub struct Problem {
-    template: Vec<char>,
+    template: HashMap<(char, char), u64>,
     rules: HashMap<(char, char), char>,
+    first: char,
+    last: char,
 }
 
 impl Problem {
@@ -14,7 +16,11 @@ impl Problem {
         let file = File::open(&path).unwrap();
         let mut lines = BufReader::new(file).lines();
 
-        let template = lines.next().unwrap().unwrap().chars().collect();
+        let mut template = HashMap::new();
+        let polymers: Vec<_> = lines.next().unwrap().unwrap().chars().collect();
+        polymers
+            .windows(2)
+            .for_each(|pair| *template.entry((pair[0], pair[1])).or_insert(0) += 1);
 
         let rules = lines
             .skip(1)
@@ -22,27 +28,47 @@ impl Problem {
             .map(|chars| ((chars[0], chars[1]), chars[6]))
             .collect();
 
-        Self { template, rules }
+        Self {
+            template,
+            rules,
+            first: *polymers.first().unwrap(),
+            last: *polymers.last().unwrap(),
+        }
     }
 
-    pub fn part_one(&self) -> u32 {
-        let polymers = (0..10).fold(self.template.clone(), |polymers, _| self.step(&polymers));
+    pub fn part_one(&self) -> u64 {
+        self.run(10)
+    }
+
+    pub fn part_two(&self) -> u64 {
+        self.run(40)
+    }
+
+    fn run(&self, steps: usize) -> u64 {
+        let pairs = (0..steps).fold(self.template.clone(), |pairs, _| self.step(&pairs));
         let mut counts = HashMap::new();
 
-        polymers
-            .iter()
-            .for_each(|polymer| *counts.entry(polymer).or_insert(0) += 1);
+        for (&(first, second), &count) in pairs.iter() {
+            *counts.entry(first).or_insert(0) += count;
+            *counts.entry(second).or_insert(0) += count;
+        }
 
-        counts.values().max().unwrap() - counts.values().min().unwrap()
+        *counts.entry(self.first).or_insert(0) += 1;
+        *counts.entry(self.last).or_insert(0) += 1;
+
+        (counts.values().max().unwrap() - counts.values().min().unwrap()) / 2
     }
 
-    fn step(&self, polymers: &[char]) -> Vec<char> {
-        let mut result: Vec<_> = polymers
-            .windows(2)
-            .flat_map(|pair| [pair[0], *self.rules.get(&(pair[0], pair[1])).unwrap()])
-            .collect();
+    fn step(&self, pairs: &HashMap<(char, char), u64>) -> HashMap<(char, char), u64> {
+        let mut result = HashMap::new();
 
-        result.push(*polymers.last().unwrap());
+        for (&pair, &count) in pairs {
+            let (first, second) = pair;
+            let new = *self.rules.get(&pair).unwrap();
+
+            *result.entry((first, new)).or_insert(0) += count;
+            *result.entry((new, second)).or_insert(0) += count;
+        }
 
         result
     }
@@ -57,5 +83,12 @@ mod tests {
         let problem = Problem::new("example.txt");
 
         assert_eq!(problem.part_one(), 1588);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let problem = Problem::new("example.txt");
+
+        assert_eq!(problem.part_two(), 2188189693529);
     }
 }

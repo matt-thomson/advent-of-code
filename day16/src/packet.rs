da6f148893f7cdb30c489e1211fd1c0f84f5e844
Matt Thomson <771863+matt-thomson@users.noreply.github.com>
2021-12-16 09:48:31 +0000

    Parse literal packets

--- day16/src/packet.rs
+++ day16/src/packet.rs
@@ -6,24 +6,42 @@ use crate::bit_stream::bits;
 pub struct Packet {
     version: u32,
     _type_id: u32,
+    value: u32,
 }
 
 impl FromStr for Packet {
     type Err = Infallible;
 
     fn from_str(input: &str) -> Result<Self, Self::Err> {
-        let bits = bits(input);
+        let mut bits = bits(input).into_iter();
 
-        let version = decimal(&bits[0..3]);
-        let type_id = decimal(&bits[3..6]);
+        let version = decimal(&mut bits.by_ref().take(3));
+        let type_id = decimal(&mut bits.by_ref().take(3));
+
+        let mut buffer = vec![];
+
+        loop {
+            let first = &bits.by_ref().next().unwrap();
+            buffer.extend(bits.by_ref().take(4));
+
+            if *first == 0 {
+                break;
+            }
+        }
+
+        let value = decimal(&mut buffer.into_iter());
 
         Ok(Self {
             version,
             _type_id: type_id,
+            value,
         })
     }
 }
 
-fn decimal(bits: &[u32]) -> u32 {
-    bits.iter().fold(0, |acc, bit| acc * 2 + bit)
+fn decimal<I>(bits: &mut I) -> u32
+where
+    I: Iterator<Item = u32>,
+{
+    bits.fold(0, |acc, bit| acc * 2 + bit)
 }

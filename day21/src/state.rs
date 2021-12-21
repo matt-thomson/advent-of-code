20ba9896ad27b6b82ec6654d6212070ff4be1ed3
Matt Thomson <771863+matt-thomson@users.noreply.github.com>
2021-12-21 19:34:47 +0000

    Day 21 part 2 (#11)

--- day21/src/state.rs
+++ day21/src/state.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, Hash, PartialEq, Eq)]
 pub struct State {
     positions: [u32; 2],
     scores: [u32; 2],
@@ -35,5 +36,23 @@ impl State {
 
     pub fn score(&self, player: usize) -> u32 {
         self.scores[player]
+    }
+
+    pub fn all(required: u32) -> Vec<Self> {
+        (0..required)
+            .flat_map(|score1| {
+                (0..required).flat_map(move |score2| {
+                    (1..=10).flat_map(move |position1| {
+                        (1..=10).flat_map(move |position2| {
+                            (0..=1).map(move |next_player| Self {
+                                positions: [position1, position2],
+                                scores: [score1, score2],
+                                next_player,
+                            })
+                        })
+                    })
+                })
+            })
+            .collect()
     }
 }

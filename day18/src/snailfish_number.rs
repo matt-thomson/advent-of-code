92097678d41f007c3bd423df62cfab2ae1d170d3
Matt Thomson <771863+matt-thomson@users.noreply.github.com>
2021-12-23 10:35:20 +0000

    Fix typos

--- day18/src/snailfish_number.rs
+++ day18/src/snailfish_number.rs
@@ -214,8 +214,7 @@ mod tests {
     #[case("[[[[3,0],[5,3]],[4,4]],[5,5]]", 791)]
     #[case("[[[[5,0],[7,4]],[5,5]],[6,6]]", 1137)]
     #[case("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]", 3488)]
-    fn should_calculate_magniture(#[case] input: SnailfishNumber, #[case] expected: u32) {
-        dbg!(&input);
+    fn should_calculate_magnitude(#[case] input: SnailfishNumber, #[case] expected: u32) {
         assert_eq!(input.magnitude(), expected);
     }
 }

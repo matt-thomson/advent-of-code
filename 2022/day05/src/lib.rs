83e1836a28a9605d582025ec718b6a537b4a6010
Matt Thomson <771863+matt-thomson@users.noreply.github.com>
2022-12-05 11:04:36 +0000

    Parse steps

--- 2022/day05/src/lib.rs
+++ 2022/day05/src/lib.rs
@@ -1,4 +1,5 @@
 mod stacks;
+mod step;
 
 use std::fs;
 use std::path::Path;
@@ -6,21 +7,27 @@ use std::path::Path;
 use eyre::{eyre, Result};
 
 use crate::stacks::Stacks;
+use crate::step::Step;
 
 #[derive(Debug)]
 pub struct Problem {
     stacks: Stacks,
+    steps: Vec<Step>,
 }
 
 impl Problem {
     pub fn new<P: AsRef<Path>>(path: P) -> Result<Self> {
         let input = fs::read_to_string(path)?;
-        let (stacks, moves) = input
+        let (stacks, steps) = input
             .split_once("\n\n")
             .ok_or_else(|| eyre!("couldn't split into stacks and moves"))?;
 
         Ok(Self {
             stacks: stacks.parse()?,
+            steps: steps
+                .lines()
+                .map(|line| line.parse())
+                .collect::<Result<Vec<_>>>()?,
         })
     }
 
---
+++ 2022/day05/src/step.rs
@@ -0,0 +1,24 @@
+use std::str::FromStr;
+
+use eyre::ErrReport;
+
+#[derive(Debug)]
+pub struct Step {
+    count: usize,
+    from: usize,
+    to: usize,
+}
+
+impl FromStr for Step {
+    type Err = ErrReport;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<_> = input.split_whitespace().collect();
+
+        let count = parts[1].parse()?;
+        let from = parts[3].parse()?;
+        let to = parts[5].parse()?;
+
+        Ok(Self { count, from, to })
+    }
+}

b66c30648f3dde60eb46c59b6a4e90711c071408
Matt Thomson <771863+matt-thomson@users.noreply.github.com>
2022-12-06 16:08:19 +0000

    Fix benchmark names

--- 2022/day06/benches/bench.rs
+++ 2022/day06/benches/bench.rs
@@ -3,12 +3,12 @@ use day06::Problem;
 
 fn part_one(c: &mut Criterion) {
     let problem = Problem::new("example.txt").unwrap();
-    c.bench_function("example", |b| b.iter(|| problem.part_one()));
+    c.bench_function("part one", |b| b.iter(|| problem.part_one()));
 }
 
 fn part_two(c: &mut Criterion) {
     let problem = Problem::new("example.txt").unwrap();
-    c.bench_function("example", |b| b.iter(|| problem.part_two()));
+    c.bench_function("part two", |b| b.iter(|| problem.part_two()));
 }
 
 criterion_group!(benches, part_one, part_two);

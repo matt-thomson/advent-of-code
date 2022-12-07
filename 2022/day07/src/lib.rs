1a5ca506e1b9e4b9c3fa43669bbcad64b3efb06b
Matt Thomson <771863+matt-thomson@users.noreply.github.com>
2022-12-07 10:01:18 +0000

    Parse input file

--- 2022/day07/src/lib.rs
+++ 2022/day07/src/lib.rs
@@ -1,1 +1,26 @@
 mod terminal_line;
+
+use std::fs;
+use std::path::Path;
+
+use eyre::Result;
+use terminal_line::TerminalLine;
+
+#[derive(Debug)]
+pub struct Problem {
+    terminal_lines: Vec<TerminalLine>,
+}
+
+impl Problem {
+    pub fn new<P: AsRef<Path>>(path: P) -> Result<Self> {
+        let input = fs::read_to_string(path)?;
+        let terminal_lines = input.lines().map(str::parse).collect::<Result<Vec<_>>>()?;
+
+        Ok(Self { terminal_lines })
+    }
+
+    pub fn part_one(&self) -> u64 {
+        dbg!(self);
+        todo!()
+    }
+}

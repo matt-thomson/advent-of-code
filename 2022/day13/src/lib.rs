c0dff813059431eb4b1c2a05cb12fb711f63a01f
Matt Thomson <771863+matt-thomson@users.noreply.github.com>
2022-12-13 11:12:22 +0000

    Parse input file

--- 2022/day13/src/lib.rs
+++ 2022/day13/src/lib.rs
@@ -1,1 +1,46 @@
 mod packet;
+
+use std::fs;
+use std::path::Path;
+
+use eyre::Result;
+
+use crate::packet::Packet;
+
+#[derive(Debug)]
+pub struct Problem {
+    packet_pairs: Vec<(Packet, Packet)>,
+}
+
+impl Problem {
+    pub fn new<P: AsRef<Path>>(path: P) -> Result<Self> {
+        let input = fs::read_to_string(path)?;
+        let lines: Vec<_> = input.lines().collect();
+        let packet_pairs: Result<Vec<(Packet, Packet)>> = lines
+            .chunks(3)
+            .map(|chunk| Ok((chunk[0].parse()?, chunk[1].parse()?)))
+            .collect();
+
+        Ok(Self {
+            packet_pairs: packet_pairs?,
+        })
+    }
+
+    pub fn part_one(&self) -> usize {
+        dbg!(self);
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Problem;
+
+    #[test]
+    fn test_part_one() {
+        let problem = Problem::new("example.txt").unwrap();
+        let result = problem.part_one();
+
+        assert_eq!(result, 13);
+    }
+}

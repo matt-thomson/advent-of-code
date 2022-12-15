373d1f209f443b4f2afb3e93d0ecb08dd6f66a14
Matt Thomson <771863+matt-thomson@users.noreply.github.com>
2022-12-15 11:06:19 +0000

    Parse input file

--- 2022/day15/src/lib.rs
+++ 2022/day15/src/lib.rs
@@ -1,2 +1,30 @@
 mod point;
 mod reading;
+
+use std::fs;
+use std::path::Path;
+
+use eyre::Result;
+
+use crate::reading::Reading;
+
+#[derive(Debug)]
+pub struct Problem {
+    readings: Vec<Reading>,
+}
+
+impl Problem {
+    pub fn new<P: AsRef<Path>>(path: P) -> Result<Self> {
+        let input = fs::read_to_string(path)?;
+        let readings: Result<Vec<Reading>> = input.lines().map(|line| line.parse()).collect();
+
+        Ok(Self {
+            readings: readings?,
+        })
+    }
+
+    pub fn part_one(&self) -> usize {
+        dbg!(self);
+        todo!()
+    }
+}
